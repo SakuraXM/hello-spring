9e11596b3812b8b3e28aa6294447e6782ead7dfe
dm <sakuram58@outlook.com>
2022-06-14 11:43:10 +0800

    【TASK】20220614 Add jdbc project

---
+++ jdbc/spring-jdbc/src/test/java/com/operation/db/demo/DataSourceDemoApplicationBackupTests.java
@@ -0,0 +1,13 @@
+package com.operation.db.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSourceDemoApplicationBackupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
