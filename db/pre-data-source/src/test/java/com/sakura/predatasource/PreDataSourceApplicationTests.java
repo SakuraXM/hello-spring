7546e6d548d4029242cdde9fdb5da0a036b07b7a
dm <sakuram58@outlook.com>
2022-06-18 18:46:52 +0800

    【TASK】20220618 Modified .gitignore file

---
+++ db/pre-data-source/src/test/java/com/sakura/predatasource/PreDataSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakura.predatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreDataSourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ db/programmatic-transaction/src/test/java/com/sakura/programmatictransaction/ProgrammaticTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakura.programmatictransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammaticTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
